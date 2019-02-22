b8a0c41906350de01b4553210752d775af38a996
Charles ORISAWAYI <CORISAWAYI@sol-tec.com>
2019-02-22 16:39:34 +0000

    updated ps1 file

--- Artifacts/windows-visualstudio2/artifact.ps1
+++ Artifacts/windows-visualstudio2/artifact.ps1
@@ -49,17 +49,11 @@ function DownloadToFilePath ($downloadUrl, $targetFile)
 Write-Output "Installing Visual Studio $version $sku"
 $logFolder = Join-path -path $env:ProgramData -childPath "DTLArt_VS"
 
-$argumentList = "--quiet --norestart --wait"
+$argumentList = "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.VisualStudioExtension --add microsoft.net.componentgroup.targetingpacks.common --add microsoft.visualstudio.component.entityframework --add microsoft.net.componentgroup.4.7.developertools --add microsoft.net.componentgroup.4.7.1.developertools --add microsoft.net.componentgroup.4.7.2.developertools --add microsoft.visualstudio.component.wcf.tooling --add microsoft.visualstudio.component.appinsights.tools --add microsoft.visualstudio.component.aspnet45 --add microsoft.visualstudio.component.webdeploy --add microsoft.visualstudio.componentgroup.iisdevelopment --add microsoft.visualstudio.web.mvc4.componentgroup --add microsoft.netcore.componentgroup.web --add microsoft.netcore.1x.componentgroup.web --add microsoft.component.azure.datalake.tools --add microsoft.visualstudio.componentgroup.azure.resourcemanager.tools --add microsoft.visualstudio.componentgroup.azure.cloudservices --add microsoft.visualstudio.component.azure.mobileappssdk --add microsoft.visualstudio.component.azure.servicefabric.tools --add microsoft.component.codeanalysis.sdk --quiet --norestart --wait"
 $downloadUrl = 'https://download.visualstudio.microsoft.com/download/pr/100196700/14dd70405e8244481b35017b9a562edd/vs_Professional.exe'
 
 $localFile = Join-Path $logFolder 'vsinstaller.exe'
 DownloadToFilePath $downloadUrl $localFile
-
-if(![String]::IsNullOrWhiteSpace($installerArgs))
-{
-    Write-Output "InstallerArgs value: $installerArgs"
-    $argumentList = "$installerArgs $argumentList"
-}
 
 Write-Output "Running install with the following arguments: $argumentList"
 $retCode = Start-Process -FilePath $localFile -ArgumentList $argumentList -Wait -PassThru
